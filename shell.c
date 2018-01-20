2f33db6660f2d344243f219829af5361a1ef7c94
Brihi <brihi16142@iiitd.ac.in>
2018-01-20 21:14:15 +0530

    made changes in shell.c

--- shell.c
+++ shell.c
@@ -11,22 +11,6 @@ Brihi Joshi
 2016142
 */
 
-/* Convert C from hexadecimal character to integer.  */
-static int
-hextobin (unsigned char c)
-{
-  switch (c)
-    {
-    default: return c - '0';
-    case 'a': case 'A': return 10;
-    case 'b': case 'B': return 11;
-    case 'c': case 'C': return 12;
-    case 'd': case 'D': return 13;
-    case 'e': case 'E': return 14;
-    case 'f': case 'F': return 15;
-    }
-}
-
 
 char* BUILTINS[5] = {"cd",
                   "echo",
