b8f4ac3f36855244484c9c6357bb19259550c7cc
Brihi <brihi16142@iiitd.ac.in>
2018-01-20 20:02:29 +0530

    finished the commands

--- shell.c
+++ shell.c
@@ -10,6 +10,24 @@
 Brihi Joshi
 2016142
 */
+
+/* Convert C from hexadecimal character to integer.  */
+static int
+hextobin (unsigned char c)
+{
+  switch (c)
+    {
+    default: return c - '0';
+    case 'a': case 'A': return 10;
+    case 'b': case 'B': return 11;
+    case 'c': case 'C': return 12;
+    case 'd': case 'D': return 13;
+    case 'e': case 'E': return 14;
+    case 'f': case 'F': return 15;
+    }
+}
+
+
 char* BUILTINS[5] = {"cd",
                   "echo",
                   "history",
@@ -76,6 +94,27 @@ int main() {
         }
         str[strlen(str)-2]= '\0';
         printf("%s",str);
+      }
+      else if (strcmp(args[1],"-e")==0){
+        char *s = args[2];
+        char c;
+        while ((c = *s++)){
+          if (c == '\\' && *s){
+              switch (c = *s++){
+                case 'a': c = '\a'; break;
+                case 'b': c = '\b'; break;
+                case 'c': return EXIT_SUCCESS;
+                case 'e': c = '\x1B'; break;
+                case 'f': c = '\f'; break;
+                case 'n': c = '\n'; break;
+                case 'r': c = '\r'; break;
+                case 't': c = '\t'; break;
+                case 'v': c = '\v'; break;
+                default:  putchar ('\\'); break;
+              }
+            }
+          putchar (c);
+        }
       }
       else{
         for (int i =1;i<temp-1;i++){
